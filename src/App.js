d88c1cf29c6405cad31279835b59aba60f08e61c
khadim24 <50873881+khadim24@users.noreply.github.com>
2024-08-20 00:50:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
-import Footer from "./components/Footer"; // Import the Footer component
+import About from ".src/components/About";
+import Contact from ".src/components/Contact";
+import Navbar from ".src/components/Navbar";
+import Projects from ".src/components/Projects";
+import Skills from ".src/components/Skills";
+import Testimonials from ".src/components/Testimonials";
+import Footer from ".src/components/Footer"; // Import the Footer component
 
 export default function App() {
   return (
