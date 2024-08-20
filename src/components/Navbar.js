8951305ddf06d8f0d88ed9e0223140910f431769
khadim24 <50873881+khadim24@users.noreply.github.com>
2024-08-19 22:29:01 -0400

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 // src/components/Navbar.js
 
-import { ArrowRightIcon } from "@npm install @heroicons/react";
-import { FaLinkedin, FaGithub } from "react-icons/fa";  // Import LinkedIn and GitHub icons
+import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export default function Navbar() {
@@ -13,7 +12,7 @@ export default function Navbar() {
             Khadim Thiam
           </a>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
             Past Work
           </a>
@@ -22,12 +21,6 @@ export default function Navbar() {
           </a>
           <a href="#testimonials" className="mr-5 hover:text-white">
             Testimonials
-          </a>
-          <a href="https://www.linkedin.com/in/khadim-thiam-4078191a9" className="mr-5 hover:text-white">
-            <FaLinkedin className="inline-block w-5 h-5 mr-1" /> LinkedIn
-          </a>
-          <a href="https://github.com/khadim24" className="mr-5 hover:text-white">
-            <FaGithub className="inline-block w-5 h-5 mr-1" /> GitHub
           </a>
         </nav>
         <a
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,6 @@
 // src/components/Skills.js
 
 import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
-import { FaPython, FaReact, FaJava, FaHtml5, FaCss3Alt, FaJs, FaFire } from "react-icons/fa";
 import React from "react";
 import { skills } from "../data";
 
@@ -22,15 +21,6 @@ export default function Skills() {
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
-  <div key={skill.name} className="p-2 sm:w-1/2 w-full">
-    <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-      {skill.icon}
-      <span className="title-font font-medium text-white ml-4">
-        {skill.name}
-      </span>
-    </div>
-  </div>
-))}
             <div key={skill} className="p-2 sm:w-1/2 w-full">
               <div className="bg-gray-800 rounded flex p-4 h-full items-center">
                 <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
@@ -38,7 +28,8 @@ export default function Skills() {
                   {skill}
                 </span>
               </div>
-            </div>   
+            </div>
+          ))}
         </div>
       </div>
     </section>
