db6067551669ffd0211a8bf0ab51c53a254e8859
khadim24 <50873881+khadim24@users.noreply.github.com>
2024-08-19 22:49:27 -0400

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+  {
+    title: "NBA Basketball Game Predictor",
+    subtitle: "Python, Machine Learning, Model Training, Web Scraping",
+    description:
+      "Developed a Machine Learning-based program in Python to predict NBA game outcomes, demonstrating proficiency in data analysis and model training",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Arcade Basketball Game",
+    subtitle: "Java, Game Design",
+    description:
+      "Designed and developed an arcade basketball game using Java, showcasing strong programming skills and creative game design principles",
+    image: "./project-2.gif",
+    link: "https://github.com/Mconde13/apcsa_modim_basketball",
+  },
+  {
+    title: "Pantry Tracker",
+    subtitle: "React, Next.js, Mui, Firebase",
+    description:
+      "Designed and developed a pantry inventory management app using React and Firebase, demonstrating expertise in full-stack development, real-time data synchronization, and user-centric design.",
+    image: "./project-3.gif",
+    link: "https://pantry-app-khadim.vercel.app/",
+  },
+  {
+    title: "Project 4",
+    subtitle: "Place Holder",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://google.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Khadim was a standout fellow at the Headstarter Summer Fellowship. Their enthusiasm and dedication were evident in every project. They tackled complex challenges with a proactive mindset and delivered exceptional results. Their passion and innovation truly shone through.",
+    image: "https://media.licdn.com/dms/image/D4E03AQHhyyJz5y9BQg/profile-displayphoto-shrink_800_800/0/1718285911693?e=1727913600&v=beta&t=pDu0jyhtOQABB1zxjdRURZFatiez8OM_Co8Vyqq3yDY",
+    name: "Yasin Ehsan",
+    company: "Headstarter",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "C++",
+  "Python",
+  "Java",
+  "Firebase",
+  "HTML/CSS"
+];
