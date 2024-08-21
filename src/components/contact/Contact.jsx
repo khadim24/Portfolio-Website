65d280a10d25d5742482a9b70e5f7e653f569ede
khadim24 <50873881+khadim24@users.noreply.github.com>
2024-08-21 02:55:00 -0400

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,21 +1,9 @@
-import React from 'react'
-import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {BsLinkedin} from 'react-icons/bs'
-import { useRef } from 'react';
-import emailjs from 'emailjs-com'
+import React from 'react';
+import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { BsLinkedin } from 'react-icons/bs';
 
 const Contact = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_3fjfb17', 'template_ky1ucaz', form.current, 'user_641J0AWGxx4qcKi835yDq')
-
-    e.target.reset()
-  };
-
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -24,20 +12,21 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>khadimth1am900@gmail.com</h5>
-            <a href="mailto:khadimth1am900@gmail.com" target="_blank">Send a message</a>
+            <a href="mailto:khadimth1am900@gmail.com" target="_blank" rel="noopener noreferrer">Send a message</a>
           </article>
           <article className="contact__option">
-            <BsLinkedin className='contact__option-icon'/>
+            <BsLinkedin className='contact__option-icon' />
             <h4>LinkedIn</h4>
             <h5>Khadim Thiam</h5>
-            <a href="https://www.linkedin.com/in/khadim-thiam-4078191a9/" target="_blank">Send a message</a>
+            <a href="https://www.linkedin.com/in/khadim-thiam-4078191a9/" target="_blank" rel="noopener noreferrer">Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form ref={form} onSubmit={sendEmail}>
+        <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" onSubmit="submit">
+          <input type="hidden" name="form-name" value="contact" />
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
@@ -45,7 +34,7 @@ const Contact = () => {
         </form>
       </div>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
